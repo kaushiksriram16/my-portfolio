a6d74dae1eb0e95bbcf3afb7285881d236d3d54d
Kaushik Sriram <47670020+kaushiksriram16@users.noreply.github.com>
2025-08-24 03:40:28 -0500

    fix page title

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ const AboutPage: React.FC = () => (
         <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-white mb-12 dark:[text-shadow:0_0_8px_rgba(255,255,255,0.4)]">About Me</h1>
         <div className="grid md:grid-cols-3 gap-8 items-center">
             <div className="md:col-span-1">
-                <img src="https://media.licdn.com/dms/image/v2/D5603AQE5R7LMxL8UrQ/profile-displayphoto-scale_200_200/B56ZeJ0ttcHQAo-/0/1750363987256?e=1757548800&v=beta&t=HJcZqDYJvHi9EcL3QpE4XdSAl3GnBUdt7_2e_nUPwbQ" alt="Your Name" className="rounded-full mx-auto shadow-lg dark:shadow-fuchsia-500/20" />
+                <img src="https://media.licdn.com/dms/image/v2/D5603AQE5R7LMxL8UrQ/profile-displayphoto-scale_200_200/B56ZeJ0ttcHQAo-/0/1750363987256?e=1757548800&v=beta&t=HJcZqDYJvHi9EcL3QpE4XdSAl3GnBUdt7_2e_nUPwbQ" alt="profile-picture" className="rounded-full mx-auto shadow-lg dark:shadow-fuchsia-500/20" />
             </div>
             <div className="md:col-span-2 text-lg text-gray-700 dark:text-gray-300 space-y-6">
                 <p>I&apos;m a passionate developer with a knack for creating elegant solutions in the least amount of time. I specialize in front-end development, particularly with React and Next.js, but I&apos;m always eager to learn new technologies and expand my skillset.</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Your Name - Portfolio</title>
+        <title>Kaushik portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
